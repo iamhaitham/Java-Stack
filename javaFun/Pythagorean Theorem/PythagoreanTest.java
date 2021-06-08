16890ecd269a0f5f968e4f9576ef679420dc9870
Haitham Estetieh <iamhaitham2011@gmail.com>
2021-06-08 20:45:31 +0200

    Added Pythagorean Theorem Assignment

---
+++ javaFun/Pythagorean Theorem/PythagoreanTest.java
@@ -0,0 +1,7 @@
+public class PythagoreanTest{
+    public static void main(String[] args){
+        Pythagorean hypotenuse=new Pythagorean();
+        double result=hypotenuse.calculateHypotenuse(3,4);
+        System.out.println(result);
+    }
+}
