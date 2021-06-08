c4f2b4aa7db05da668d5b1092902029739b5637b
Haitham Estetieh <iamhaitham2011@gmail.com>
2021-06-08 21:44:36 +0200

    Added FizzBuzz Assignment

---
+++ javaFun/FizzBuzz/FizzBuzzTest.java
@@ -0,0 +1,7 @@
+public class FizzBuzzTest{
+    public static void main(String[] args){
+        FizzBuzz check=new FizzBuzz();
+        String result=check.fizzBuzz(8);
+        System.out.println(result);
+    }
+}
