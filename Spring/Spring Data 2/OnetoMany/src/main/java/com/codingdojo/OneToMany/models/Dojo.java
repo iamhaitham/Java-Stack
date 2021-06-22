5d3a67f7a0a119d49ba077d18cdb49cf4f6aa657
Haitham Estetieh <iamhaitham2011@gmail.com>
2021-06-22 13:32:07 +0200

    Prepared some files for One to Many Assignment

---
+++ Spring/Spring Data 2/OnetoMany/src/main/java/com/codingdojo/OneToMany/models/Dojo.java
@@ -0,0 +1,87 @@
+package com.codingdojo.OneToMany.models;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="dojos")
+public class Dojo {
+	 @Id
+	    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	    private Long id;
+	    private String name;
+	    @Column(updatable=false)
+	    private Date createdAt;
+	    private Date updatedAt;
+	    @OneToMany(mappedBy="dojo", fetch = FetchType.LAZY)
+	    private List<Ninja> ninjas;
+	    
+	    public Dojo() {
+	        
+	    }
+	    
+	    
+	    
+	    public String getName() {
+			return name;
+		}
+
+
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+
+
+		public List<Ninja> getNinjas() {
+			return ninjas;
+		}
+
+
+
+		public void setNinjas(List<Ninja> ninjas) {
+			this.ninjas = ninjas;
+		}
+
+
+
+		public Long getId() {
+			return id;
+		}
+
+
+
+		public Date getCreatedAt() {
+			return createdAt;
+		}
+
+
+
+		public Date getUpdatedAt() {
+			return updatedAt;
+		}
+
+
+
+		@PrePersist
+		public void setCreatedAt(Date createdAt) {
+			this.createdAt = new Date();
+		}
+	    
+	    @PreUpdate
+		public void setUpdatedAt(Date updatedAt) {
+			this.updatedAt = new Date();
+		}
+}
---
+++ Spring/Spring Data 2/OnetoMany/src/test/java/com/codingdojo/OneToMany/OnetoManyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingdojo.OneToMany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnetoManyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
