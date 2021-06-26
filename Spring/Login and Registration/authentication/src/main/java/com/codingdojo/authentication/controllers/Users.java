6b6cfeb9d31778eb9d63506fba913712b143ef8f
Haitham Estetieh <iamhaitham2011@gmail.com>
2021-06-26 17:58:45 +0200

    Added authentication assignment

---
+++ Spring/Login and Registration/authentication/src/main/java/com/codingdojo/authentication/controllers/Users.java
@@ -0,0 +1,81 @@
+package com.codingdojo.authentication.controllers;
+
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.codingdojo.authentication.models.User;
+import com.codingdojo.authentication.services.UserService;
+
+@Controller
+public class Users {
+    private final UserService userService;
+    public Users(UserService userService) {
+        this.userService = userService;
+    }
+    
+    @RequestMapping("/registration")
+    public String registerForm(@ModelAttribute("user") User user) {
+        return "registrationPage.jsp";
+    }
+    @RequestMapping("/login")
+    public String login() {
+        return "loginPage.jsp";
+    }
+    
+    @RequestMapping(value="/registration", method=RequestMethod.POST)
+    public String registerUser(@Valid @ModelAttribute("user") User user, BindingResult result, HttpSession session) {
+    	// if result has errors, return the registration page (don't worry about validations just now)
+        // else, save the user in the database, save the user id in session, and redirect them to the /home route
+    	if(result.hasErrors()) {
+    		return "redirect:/registration";
+    	}else {
+    		User newUser=userService.registerUser(user);
+    		session.setAttribute("user_id",newUser.getId());
+    		return "redirect:/home";
+    	}
+    	
+    }
+    
+    @RequestMapping(value="/login", method=RequestMethod.POST)
+    public String loginUser(@RequestParam("email") String email, @RequestParam("password") String password, Model model, HttpSession session,RedirectAttributes redirectAttributes) {
+        // if the user is authenticated, save their user id in session
+        // else, add error messages and return the login page
+    	boolean authentication_result=userService.authenticateUser(email,password);
+    	if(authentication_result==false) {
+    		redirectAttributes.addFlashAttribute("error","Login information didn't match our records! Please, double-check it again.");
+    		return "redirect:/login";
+    	}else {
+    		session.setAttribute("user_id",userService.findByEmail(email).getId());
+    		return "redirect:/home";
+    	}
+    }
+    
+    @RequestMapping("/home")
+    public String home(HttpSession session, Model model) {
+        // get user from session, save them in the model and return the home page
+    	if(session.getAttribute("user_id")==null){
+    		return "You didn't register or log in!";
+    	}else {
+    	Long myUserId=(Long) session.getAttribute("user_id");
+    	User myUser=userService.findUserById(myUserId);
+    	model.addAttribute("user",myUser);
+    	return "homePage.jsp";
+    	}
+    }
+    @RequestMapping("/logout")
+    public String logout(HttpSession session) {
+        // invalidate session
+        // redirect to login page
+        session.invalidate();
+        return "redirect:/login";
+    }
+}
---
+++ Spring/Login and Registration/authentication/src/main/java/com/codingdojo/authentication/models/User.java
@@ -0,0 +1,76 @@
+package com.codingdojo.authentication.models;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+@Entity
+@Table(name="users")
+public class User {
+	
+	@Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String password;
+    @Transient
+    private String passwordConfirmation;
+    @Column(updatable=false)
+    private Date createdAt;
+    private Date updatedAt;
+
+    // ****** CONSTRUCTOR ******
+    public User() {
+    }
+    
+    // ****** GETTERS ******
+    public Long getId() {
+		return id;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+	public String getPasswordConfirmation() {
+		return passwordConfirmation;
+	}
+
+    // ****** SETTERS ******
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public void setPasswordConfirmation(String passwordConfirmation) {
+		this.passwordConfirmation = passwordConfirmation;
+	}
+	
+	@PrePersist
+    protected void onCreate(){
+        this.createdAt = new Date();
+    }
+
+	@PreUpdate
+    protected void onUpdate(){
+        this.updatedAt = new Date();
+    }
+}
